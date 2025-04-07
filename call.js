1e9b869503a97d8b464f2b0c301f82d0f7bf0718
Leon Chou <iamyiii0328@gmail.com>
2025-04-07 22:40:03 +0800

    修改自定義call方法

--- call.js
+++ call.js
@@ -3,17 +3,20 @@ Function.prototype.selfCall = function (thisArg, ...args) {
   console.log("selfCall被執行了");
 
   // 1. 獲取需要被執行的函數
-  var fn = this; // 利用隱式綁定特性，知道 this 就會是該函數
+  let fn = this; // 利用隱式綁定特性，知道 this 就會是該函數
 
   // 2. 將 thisArg 轉換為物件類型
   thisArg = thisArg ? Object(thisArg) : window; // 如果 thisArg 是 null 或 undefined，則設置為 window
 
   // 3. 調用需要被執行的函數
-  thisArg.fn = fn;
-  thisArg.fn(...args);
+  thisArg._fn = fn;
+  let result = thisArg._fn(...args);
 
-  // 刪除 thisArg 上的 fn 屬性
-  delete thisArg.fn;
+  // 4. 刪除 thisArg 上的 fn 屬性
+  delete thisArg._fn;
+
+  // 5. 返回函數的執行結果
+  return result;
 };
 
 function foo() {
